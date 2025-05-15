9972d0d46b1d6e0b3f7b4b4fd3904f2dc40934fe
NN <nicolasnunezlira@gmail.com>
2025-05-15 12:51:02 -0400

    square grid without bounds

---
+++ Assets/Scripts/Procedural Meshes/IMeshGenerator.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+						
+namespace ProceduralMeshes {
+
+	public interface IMeshGenerator {
+        int VertexCount {get; }
+
+        int IndexCount {get;}
+
+        int JobLength {get;}
+
+		void Execute<S> (int i, S streams) where S : struct, IMeshStreams;
+	}
+}
